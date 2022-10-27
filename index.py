676bd178d6692733273bcd23261ade5b1f416c0c
SantaRiver <santariver@yandex.ru>
2022-10-27 14:11:41 +0500

    db and static path

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import render_template
-from flask import url_for
+from db.app import Tickets
+from db.app import db
+from flask import request
 
 app = Flask(__name__, static_url_path="/", static_folder='templates')
 
@@ -8,6 +10,25 @@ app = Flask(__name__, static_url_path="/", static_folder='templates')
 @app.route("/")
 def index():
     return render_template('/index.html')
+
+
+@app.route("/ticket/<seed>", )
+def ticket(seed):
+    ticket_el = Tickets.query.filter_by(seed=seed).first_or_404()
+    return str(ticket_el)
+    # print(tickets)
+
+
+@app.route("/ticket", methods=['POST'])
+def create_ticket():
+    if request.method == 'POST':
+        ticket_el = Tickets('123', '123', '123')
+        db.session.add(ticket_el)
+        db.session.commit()
+    else:
+        return 'else'
+
+    return 'asd'
 
 
 if __name__ == '__main__':
