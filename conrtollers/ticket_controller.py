344baed78a77aee39a31ef76c259ea07310cb82c
SantaRiver <santariver@yandex.ru>
2022-11-01 15:23:39 +0500

    filter in tickets/

--- conrtollers/ticket_controller.py
+++ conrtollers/ticket_controller.py
@@ -1,8 +1,10 @@
+import sqlalchemy
 from sqlalchemy import exc
-from flask import request, make_response, jsonify
+from flask import request
 from helpers.json import json_response
 from models.ticket import Ticket
 from database.connect import db_session
+from database.connect import Base
 
 
 def create(req: request):
@@ -23,9 +25,14 @@ def create(req: request):
 
 
 def read(req: request):
-    tickets = Ticket.query.all()
-    response = json_response(tickets)
-    return response
+    args = req.args.to_dict()
+
+    try:
+        tickets = Ticket.query.filter_by(**args).all()
+    except exc.InvalidRequestError as e:
+        return json_response(str(e), 'error', 500)
+
+    return json_response(tickets)
 
 
 def update(req: request):
